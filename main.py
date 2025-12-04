6c74c1f0b178a35f134862b9c3cff3b42c6fe0a1
Miso2233 <1015789185@qq.com>
2025-12-04 20:00:40 +0800

    修改了不符合规范的类名 完善了战场信息打印 构建了Al4咆哮 将一天划分为数个时段 加入大量Al_general方法接口 在每个时间节点利用鸭子类型进行Al方法的执行 写入了所有Al的元数据

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Voices:
                 txt = random.choice(cls.voices[who][theme])
             Txt.printplus(txt)
         except KeyError:
-            pass
+            print(f"语音未定义-[{who}]{theme}")
 
 class Dice:
     """
@@ -63,82 +63,18 @@ class Dice:
             return 0
 
     @staticmethod
-    def probability(pro) -> bool:  # 概率为真
+    def probability(pro:float) -> bool:
+        """
+        这一函数有pro概率为真，反之为假
+        :param pro: 概率值，介于0和1之间
+        :return:
+        """
         if random.random() < pro:
             return True
         else:
             return False
 
-class Al_manager:
-    file_path = os.path.join('resources', 'al_meta_data.json')
-    with open(file_path, 'r', encoding='utf-8') as f:
-        al_meta_data: dict[str:any] = json.load(f)
-
-class Al_general:
-    #Apocalypse-Linked 明日尘埃装备体系
-
-    def __init__(self,index:int):
-        # metadata 字段
-        self.index:int                  = index
-        self.short_name:str             = Al_manager.al_meta_data[str(index)]["short_name"]
-        self.len_name:str               = Al_manager.al_meta_data[str(index)]["len_name"]
-        self.type:Literal["q","w","e"]  = Al_manager.al_meta_data[str(index)]["type"]
-        self.rank_num:int               = Al_manager.al_meta_data[str(index)]["rank_num"]
-        self.metadata:dict[str:str|int] = Al_manager.al_meta_data[str(index)]
-
-        # operation 字段
-        self.state = 0
-
-    def report(self, theme:str):
-        """
-        Al所包装的说话函数，省去了说话者名字
-        :param theme: 主题
-        :return: 无
-        """
-        Voices.report(self.short_name,theme)
-
-    def add_atk(self,atk:int):
-        """
-        为atk提供加成
-        :param atk: 加成前atk
-        :return: 加成后atk
-        """
-        return atk
-
-    def add_atk(self,hp:int):
-        """
-        为hp提供加成
-        :param hp: 加成前hp
-        :return: 加成后hp
-        """
-        return hp
-
-    def operate_morning(self):
-        pass
-
-    def operate_afternoon(self):
-        pass
-class Al3(Al_general):
-
-    def react(self):
-        if Dice.probability(0.3*enemy.shelter):
-            my_ship.attack(1)
-            self.report("命中")
-        else:
-            self.report("未命中")
-al3 = Al3(3)
-
-
-class Printer:
-
-    @classmethod
-    def print_for_fight(cls, me:My_ship, enemy:Enemy_ship):
-        enemy.print_self()
-        print("\n\n\n")
-        me.print_self()
-        print("\n")
-
-class My_ship:
+class MyShip:
     """
     ship.print_self()
     ship.attack(3,enemy)
@@ -148,17 +84,23 @@ class My_ship:
     def __init__(self):
         self.shelter = 0
         self.missile = 0
-        self.al_list = [None,None,al3]
+        self.al_list:list[Al_general|None] = [None,None,None]
 
     def print_self(self):
         for _ in range(self.shelter):
             print("-----")
         for _ in range(self.missile):
             print("[]",end="")
+        print()
 
     def initialize(self):
         self.missile = 1
         self.shelter = 1
+        for al in self.al_list:
+            try:
+                al.initialize()
+            except AttributeError:
+                pass
 
     def attack(self,atk:int):
         """
@@ -203,13 +145,17 @@ class My_ship:
             case "2":
                 self.heal(1)
                 Voices.report("护盾","上盾")
+            case "q":
+                self.al_list[0].react()
+            case "w":
+                self.al_list[1].react()
             case "e":
                 self.al_list[2].react()
             case _:
                 Txt.printplus("你跳过了这一天！")
-my_ship = My_ship()
+my_ship = MyShip()
 
-class Enemy_ship:
+class EnemyShip:
     def __init__(self):
         self.shelter = 0
         self.missile = 0
@@ -263,12 +209,152 @@ class Enemy_ship:
                 self.heal(1)
             case _:
                 Txt.printplus("敌人跳过了这一天！")
+enemy = EnemyShip()
 
-enemy = Enemy_ship()
+class Al_manager:
+    file_path = os.path.join('resources', 'al_meta_data.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        al_meta_data: dict[str:dict[str:str|int]] = json.load(f)
 
-class Main_loops:
+class Al_general:
+    #Apocalypse-Linked 明日尘埃装备体系
+
+    def __init__(self,index:int):
+        # metadata 字段
+        self.index:int                  = index
+        self.short_name:str             = Al_manager.al_meta_data[str(index)]["short_name"]
+        self.len_name:str               = Al_manager.al_meta_data[str(index)]["len_name"]
+        self.type:Literal["q","w","e"]  = Al_manager.al_meta_data[str(index)]["type"]
+        self.rank_num:int               = Al_manager.al_meta_data[str(index)]["rank_num"]
+        self.skin_list:list[str]        = Al_manager.al_meta_data[str(index)].get("skin_list",[])
+        self.metadata:dict[str:str|int] = Al_manager.al_meta_data[str(index)]
+
+        # operation 字段
+        self.state = 0
+
+    def report(self, theme:str):
+        """
+        Al所包装的说话函数，省去了说话者名字
+        :param theme: 主题
+        :return: 无
+        """
+        Voices.report(self.short_name,theme)
+
+    def add_atk(self,atk:int):
+        """
+        为atk提供加成
+        :param atk: 加成前atk
+        :return: 加成后atk
+        """
+        return atk
+
+    def add_hp(self,hp:int):
+        """
+        为hp提供加成
+        :param hp: 加成前hp
+        :return: 加成后hp
+        """
+        return hp
+
+    def operate_morning(self):
+        pass
+
+    def operate_afternoon(self):
+        pass
+
+    def initialize(self):
+        self.state = 0
+
+    def print_self(self):
+        if self.state != 0:
+            try:
+                print(self.skin_list[self.state])
+                print()
+            except IndexError:
+                pass
+class Al3(Al_general):
+
+    def react(self):
+        if Dice.probability(0.3*enemy.shelter):
+            my_ship.attack(1)
+            self.report("命中")
+        else:
+            self.report("未命中")
+al3 = Al3(3)
+
+class Al4(Al_general):
+
+    def react(self):
+        if self.state<2:
+            self.state+=2
+            self.report("收到")
+            time.sleep(0.4)
+            if self.state == 2:
+                self.report("准备好")
+
+    def operate_afternoon(self):
+        if self.state>=2:
+            self.state+=1
+            if self.state==4:
+                self.state=0
+            if Dice.probability(0.7):
+                my_ship.attack(1)
+                self.report("命中")
+            else:
+                self.report("未命中")
+                my_ship.load(1)
+                self.report("回流")
+al4 = Al4(4)
+
+class FieldPrinter:
+
+    @classmethod
+    def print_for_fight(cls, me:MyShip, opposite:EnemyShip):
+        """
+        打印双方护盾和导弹，以及我方Al
+        :param me:
+        :param opposite:
+        :return: 无
+        """
+        opposite.print_self()
+        print("\n\n\n")
+        try:
+            me.al_list[1].print_self()
+        except AttributeError:
+            pass
+        me.print_self()
+        try:
+            me.al_list[2].print_self()
+        except AttributeError:
+            pass
+        try:
+            me.al_list[0].print_self()
+        except AttributeError:
+            pass
+        print("\n")
+
+    @classmethod
+    def print_basic_info(cls,days):
+        """
+        打印战场基本信息
+        :param days: 当前天数
+        :return: 无
+        """
+        print("~~~~~~~~~~~~~~~~~~~~~~~~")
+        Txt.printplus(f"指挥官，今天是战线展开的第{days}天。",0)
+        if days < 5:
+            Txt.printplus("当前舰船位置>>正在离港")
+        elif days < 10:
+            Txt.printplus("当前舰船位置>>我方领土边缘")
+        elif days <= 20:
+            Txt.printplus("当前舰船位置>>边境核心战场")
+        elif days > 20:
+            Txt.printplus("当前舰船位置>>敌方腹地危险区域")
+
+class MainLoops:
 
     days = 0
+    key_prompt = ""
 
     @staticmethod
     def is_over() -> Literal[-1,0,1]:
@@ -288,13 +374,15 @@ class Main_loops:
         enemy.initialize()
         cls.days = 1
 
+
+
     @classmethod
     def fight_mainloop(cls):
         while 1:
             # dawn
             time.sleep(0.4)
-            Txt.printplus(f"指挥官，今天是战线展开的第{cls.days}天。")
-            Printer.print_for_fight(my_ship, enemy)
+            FieldPrinter.print_basic_info(cls.days)
+            FieldPrinter.print_for_fight(my_ship, enemy)
 
             # morning
             for al in my_ship.al_list:
@@ -325,5 +413,6 @@ class Main_loops:
             cls.days += 1
 
 if __name__ == "__main__":
-    Main_loops.initialize_before_fight()
-    Main_loops.fight_mainloop()
+    my_ship.al_list = [al4,None,al3]
+    MainLoops.initialize_before_fight()
+    MainLoops.fight_mainloop()
